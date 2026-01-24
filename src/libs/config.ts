d4ac7bca908c617ce8c981a169a5b55c886f0e75
Muhammad Nurul Huda <nu12ulhud4@gmail.com>
2026-01-24 21:13:23 +0700

    fix: config

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,11 +1,11 @@
 const localConfig = {
-  dbUrl: 'postgresql://postgres.rzcqsirfrhvgnsqligbe:9a7175e6dd5b2cbcaf85d5333e631475aab9fea5cc6bdfebfc966d0fdf71a806@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres',
+  dbUrl: 'postgresql://postgres.bcxpamrajlziynpkurul:JjuXe6NOu8YvfULZ@aws-1-ap-south-1.pooler.supabase.com:6543/postgres',
   jwt: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im1yay00MzAyZWMxYjY3MGY0OGE5OGFkNjFkYWRlNGEyM2JlNyJ9',
-  supabaseUrl: 'https://rzcqsirfrhvgnsqligbe.supabase.co',
-  supabaseAnonKey: 'sb_publishable_L3nVmqiMZGggb-bxMhAoNw_Q8f0EdNa',
+  supabaseUrl: 'https://bcxpamrajlziynpkurul.supabase.co',
+  supabaseAnonKey: 'sb_publishable_m57sPVe3UAmTEOCNKahLng_SsYvLqQ7',
   basic: {
-    user: 'basicposuser',
-    pass: 'basicpospass'
+    user: 'restaurantApi',
+    pass: 'JjuXe6NOu8YvfULZ'
   },
 }
 
