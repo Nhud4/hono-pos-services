e85df4c62964146f8164653285af6a365af67873
Muhammad Nurul Huda <nu12ulhud4@gmail.com>
2026-01-24 20:23:34 +0700

    fix: authrization on users api

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,14 +3,15 @@ import { basicAuth } from '../middlewares/basic-auth';
 import { validate } from '../middlewares/validate';
 import * as handler from '../handlers/user.handler';
 import * as schema from '../validators/user.validator'
+import { jwtAuth } from '../middlewares/jwt-auth';
 
 const router = new Hono();
 
-router.get('/users', basicAuth(), validate('query', schema.listUserSchema), handler.getAllUsersHandler);
-router.get('/users/:id', basicAuth(), validate('param', schema.userIdSchema), handler.getUserByIdHandler);
+router.get('/users', jwtAuth(), validate('query', schema.listUserSchema), handler.getAllUsersHandler);
+router.get('/users/:id', jwtAuth(), validate('param', schema.userIdSchema), handler.getUserByIdHandler);
 router.post('/users', basicAuth(), validate('json', schema.createUserSchema), handler.createUserHandler);
-router.put('/users/:id', basicAuth(), validate('json', schema.createUserSchema), handler.updateUserHandler);
-router.delete('/users/:id', basicAuth(), validate('param', schema.userIdSchema), handler.deleteUserHandler);
+router.put('/users/:id', jwtAuth(), validate('json', schema.createUserSchema), handler.updateUserHandler);
+router.delete('/users/:id', jwtAuth(), validate('param', schema.userIdSchema), handler.deleteUserHandler);
 
 // Auth routes
 router.post('/auth/login', basicAuth(), validate('json', schema.loginSchema), handler.loginHandler);
